ef0e4c87bf5ff6277b94b143a17e494bd04de181
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-02-06 22:54:10 -0800

    Time: 122 ms (81.72%), Space: 47.5 MB (12.95%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -11,22 +11,7 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    /*
-        I: Two non empty linked lists representing two non-neg ints 
-           - Stored in reverse order
-           - Each node contains a single didgit 
-           
-        O: Another linked list stored in reverse order
-        C: Int sized output
-        
-        E: May have to carry the one over and make the list longer
-        
-        
-        For best runtime we want to use one iteration over both of the lists, 
-        We would prefer to use the longest list
-    */
     let head = l1;
-    console.log(`Head ${head.val}`);
     
     let inner = (l1, l2, remainder) => {
         let total = l1.val + l2.val + remainder;
@@ -41,17 +26,14 @@ var addTwoNumbers = function(l1, l2) {
         l1.val = total;
         
         if (l1.next === null && l2.next !== null || l1.next === null && remainder !== 0) {
-            console.log("hit this case");
             l1.next = new ListNode(0, null);
         }
         
         if (l2.next === null && l1.next !== null || l1.next === null && remainder !== 0) {
-            console.log("Hit that case");
             l2.next = new ListNode(0, null);
         }
         
         if (l1.next === null && l2.next === null) {
-            console.log("hit edge case");
             return head;
         }
         
