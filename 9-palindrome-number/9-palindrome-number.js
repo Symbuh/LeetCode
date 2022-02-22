a738e8390d8a3a27bb6f4fc100f63a0d7feb9e02
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-02-21 17:45:25 -0800

    Time: 263 ms (56.61%), Space: 51.3 MB (22.02%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,21 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    /*
-        123321
-        iceyeci
-        
-          
-    */
-    
+
     x = x.toString();
     let subStr1, subStr2;
-    console.log('length');
-    console.log(x.length / 2)
-    console.log(Math.ceil(x.length) / 2)
     subStr1 = x.slice(0, Math.floor(x.length / 2))
     subStr2 = x.slice(Math.ceil(x.length) / 2, x.length)
-    console.log(subStr1 + ' ' +  subStr2)
     
     if (subStr1.length === subStr2.length) {
         if (subStr1 === subStr2.split('').reverse().join('')) {
