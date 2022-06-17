b4fe83d2fc3c4f4e29b4acaa2c5e30b10f6b423e
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-06-16 19:48:40 -0700

    Time: 79 ms (54.19%), Space: 42.5 MB (13.40%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.js
@@ -12,20 +12,6 @@
 
 
 var swapPairs = function(head) {
-    /*
-        Iterate through the list and swap every other value 
-        
-        we must remake head at the begenning each time
-        
-        To swap properly while preserving the list: 
-            - left.next = right.next
-            - right.next = left
-            - head = right. 
-    */
-    
-    
-    // Init logic, as head only needs to be redefined once. 
-    
     
     if (!head || !head.next) {
         return head;
@@ -42,7 +28,6 @@ var swapPairs = function(head) {
     
     // Making use of the Optional Chaining operator in order to handle this case. 
     while (!!node?.next) {
-        console.log(`Entering while, Node: ${node.val}, Next: ${node.next.val}`)
         temp = node.next;
         node.next = node.next.next;
         temp.next = node;
