da4dc2860b316a12838bfba6d2a3ead056b21953
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-06-16 21:29:44 -0700

    Time: 102 ms (21.58%), Space: 41.8 MB (86.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -6,45 +6,45 @@
 
 var search = function(nums, target) {
     // Start with a direct check 
-    if (nums[target] === target) {
-        return target;
-    }
+//     if (nums[target] === target) {
+//         return target;
+//     }
     
-    // Logic to check for a pivot
-    let findPivot = (nums) => {
-        for (let i = 0; i < nums.length; i++) {
-            if (nums[i] === 0) {
-                return i;
-            }
-        }
+//     // Logic to check for a pivot
+//     let findPivot = (nums) => {
+//         for (let i = 0; i < nums.length; i++) {
+//             if (nums[i] === 0) {
+//                 return i;
+//             }
+//         }
         
-        return -1;
-    }
+//         return -1;
+//     }
     
-    let pivot = findPivot(nums);
+//     let pivot = findPivot(nums);
     
-    if (pivot !== -1) {
-        /*
-            This is where we can get really fancy to go as directly to our target as possible. 
+//     if (pivot !== -1) {
+//         /*
+//             This is where we can get really fancy to go as directly to our target as possible. 
         
-            if the target is greater than our pivot index it will reside on the left side of our zero
-                its index will be target - pivot.
+//             if the target is greater than our pivot index it will reside on the left side of our zero
+//                 its index will be target - pivot.
             
-            if the target is less than our pivot it will be on the right
-                its index will be pivot + target
+//             if the target is less than our pivot it will be on the right
+//                 its index will be pivot + target
             
-        */
+//         */
         
-        if (target > pivot) {
-            if (nums[target - pivot] === target) {
-                return (target - pivot)
-            }
-        } else {
-            if (nums[target + pivot] === target) {
-                return target + pivot
-            }
-        }
-    }
+//         if (target > pivot) {
+//             if (nums[target - pivot] === target) {
+//                 return (target - pivot)
+//             }
+//         } else {
+//             if (nums[target + pivot] === target) {
+//                 return target + pivot
+//             }
+//         }
+//     }
 
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === target) {
