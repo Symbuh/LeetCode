ff3862d8dae47d1a61df61ce76c3688037b3fb73
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-06-20 12:51:40 -0700

    Time: 106 ms (61.20%), Space: 44.7 MB (11.39%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -19,7 +19,6 @@ var twoSum = function(nums, target) {
     
     for (let i = 0; i < nums.length; i++) {
         diff = target - nums[i]
-        console.log(`nums[i]: ${nums[i]} Diff: ${diff} memo[diff]: ${memo[diff]}`)
         if (memo[diff] && i !== memo[diff]) {
             return i > memo[diff] ? [memo[diff], i] : [i, memo[diff]]
         }
