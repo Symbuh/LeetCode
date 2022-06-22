1b88b9f422d626a0ab02b3f5827c3dfcb47aed23
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-06-22 09:11:13 -0700

    Time: 140 ms (43.67%), Space: 51.3 MB (32.43%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,15 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
-    /*
-        this is pretty slow I think 
-        
-        We shouldn't have to sort it we just add each value to a memo as we walk through it 
-        
-        Maybe memoization is a core concept of programming
-    */
-    
     let memo = {}
     
     for (let i = 0; i < nums.length; i++) {
