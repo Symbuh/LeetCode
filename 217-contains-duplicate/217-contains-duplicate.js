219e6f0010bdba4e9de66efa269cd26fbe504ab4
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-06-22 09:05:29 -0700

    Time: 212 ms (22.36%), Space: 51.1 MB (44.88%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    nums = nums.sort();
+    
+    for (let i = 1; i < nums.length; i++) {
+        if(nums[i-1] === nums[i]) {
+            return true;
+        }
+    }
+    return false;
+};
