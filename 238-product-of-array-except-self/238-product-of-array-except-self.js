17caec68efeb3091e9dd19772b85c24bee727cb2
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-06-23 18:32:15 -0700

    Time: 160 ms (42.00%), Space: 54.7 MB (51.84%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,17 +3,32 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let returnArray = [];
-    let current = 0;
-    let thing = 1;
-    for (let i = 0; i < nums.length; i++) {;
-        for(let j = 0; j < nums.length; j++) {
-            if(j !== i){
-                thing = thing*nums[j];
-            }
-        }
-        returnArray.push(thing);
-        thing = 1;
+    /*
+        I didn't come up with this solution on my own. 
+        
+        We're going to be constructing an array representing 
+        every character to the left of our current index being multiplied together
+        
+        We will then start from the end of the array and construct our output through multiplying.
+        whatever's to the right of our index to the multiplicands on the left for that particular indicie. 
+    */
+    
+    let output = []
+    let right = 1
+    
+    output[0] = 1
+    
+    for (let i = 1; i < nums.length; i++) {
+        // output i is the product of all values in nums that come before nums[i]
+        output[i] = nums[i-1] * output[i-1]
     }
-    return returnArray;
+    
+    // Now we just need to repeat this process for everything to the right of or after nums[i]
+    // We can multiply these with the values stored in output[i] to get the values we're looking for. 
+    for (let i = nums.length - 1; i >= 0; i--) {
+        output[i] *= right
+        right *= nums[i]
+    }
+    
+    return output;
 };
