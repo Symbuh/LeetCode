9d6fb05f105385368db24420869c54e9d8297c42
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-08-27 04:10:27 -0700

    Time: 3 ms (36.27%), Space: 2.2 MB (15.55%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.go
+++ 20-valid-parentheses/20-valid-parentheses.go
@@ -1,23 +1,10 @@
 func isValid(s string) bool {
-    /*
-        We need to store some sort of queue
-        of the open brackets
-        
-        Last in last out to maintain order 
-        
-        if we find another opening bracket push it
-        
-        if the closing bracket we find corresponds
-        to an opening bracket we are free to push it out
-        of this queue
-        
-        if we have anything remaining in the queue, 
-        or ever parse through a closing bracket while 
-        the queue is empty, we can return false. 
-    */
     
-    var validChars = 
-    map[string]string{"{":"}", "(":")", "[":"]"}
+    var validChars = map[string]string{
+        "{":"}", 
+        "(":")", 
+        "[":"]",
+    }
     
     var queue []string 
     
@@ -27,7 +14,6 @@ func isValid(s string) bool {
         if validChars[char] != "" {
             queue = append(queue, validChars[char])
         } else {
-            // If we don't get an opening bracket: 
             if len(queue) == 0 || i == 0 {
                 return false
             }
