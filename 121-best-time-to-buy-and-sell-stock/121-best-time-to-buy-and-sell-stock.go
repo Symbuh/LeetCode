991afeab32f5a57eec8fcbe0bab0b8b45442cbfc
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-09-12 15:12:12 -0700

    Time: 270 ms (18.33%), Space: 9 MB (44.62%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
@@ -1,24 +1,14 @@
 func maxProfit(prices []int) int {
-    /*
-        The vars we'll need to buy and sell this are max profit and currMin
-        
-        We want to update currMin as we parse through the array and update max profit as we find values. 
-        
-        Return max profit, super simple
-    */
-    
     var maxProfit = 0
     var currMin = math.MaxUint32
     
     for i := 0; i < len(prices); i++ {
         if (prices[i] < currMin) {
             currMin = prices[i]
-            fmt.Println(currMin)
         }
         
         if (prices[i] > currMin && prices[i] - currMin > maxProfit) {
             maxProfit = prices[i] - currMin
-            fmt.Println(maxProfit)
         }
     }
     
